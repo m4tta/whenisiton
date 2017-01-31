d48c8364cf84f83969c60aeeadc566e15b839f35
m4tta <matt@fizzled.me>
2017-01-31 00:00:28 -0800

    more style reorganization

--- client/Containers/Layout.js
+++ client/Containers/Layout.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 class Layout extends Component {
 
   render() {
     return (
       <div>
-        <div className="container">
-          <a  className="brand">When Is It On</a>
+        <div className="nav">
+          <Link to='/' className="brand">When Is It On</Link>
         </div>
         {this.props.children}
       </div>
