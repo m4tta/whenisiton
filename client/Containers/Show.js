7931567b80b648d014cda82b59bb8823c2012dbd
m4tta <matt@fizzled.me>
2017-01-31 00:10:28 -0800

    wired up show details

--- client/Containers/Show.js
+++ client/Containers/Show.js
@@ -36,11 +36,32 @@ class Show extends React.Component {
 
   render() {
     const show = this.state.show;
+    let genres;
+    if (show.genres) {
+      genres = show.genres.map((genre, index) => {
+        return (<div key={index} className="genre">{genre.name}</div>)
+      });
+    }
+
     return (
       <div className="container -show">
         <div className="details">
           <img src={`http://image.tmdb.org/t/p/w342${show.poster_path}`} alt={show.name} className="poster"/>
-
+          <div className="details-content">
+            <div className="show-name">{show.name}</div>
+            <div className="overview">
+              <div className="header">Overview</div>
+              <div className="body -well">{show.overview}</div>
+            </div>
+            <div className="next-up">
+            </div>
+            <div className="genres">
+              <div className="header">Genres</div>
+              <div className="list">
+                {genres}
+              </div>
+            </div>
+          </div>
         </div>
         <div className="details-extra">
 
