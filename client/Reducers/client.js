877165f66b32d6ff68b6dddd2919d1a631e27b84
m4tta <matt@fizzled.me>
2017-02-04 00:35:15 -0800

    fixed client reducers to not kill old state

--- client/Reducers/client.js
+++ client/Reducers/client.js
@@ -2,11 +2,13 @@ export default function client(state = {}, action) {
   switch (action.type) {
     case 'SET_BACKGROUND':
       return {
+        ...state,
         backgroundUrl: action.backgroundUrl,
       }
       break;
     case 'SET_PAGETITLE':
       return {
+        ...state,
         pageTitle: action.title,
       }
       break;
