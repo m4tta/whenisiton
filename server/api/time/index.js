ba20186a2d18d9549f196bcc0a4c8daaa94c7994
m4tta <matt@fizzled.me>
2017-02-07 00:06:59 -0800

    removed unused code

--- server/api/time/index.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var controller = require('./time.controller');
-var router = express.Router();
-
-router.get('/time', controller.time);
-
-module.exports = router;
--- server/api/time/time.controller.js
+++
@@ -1,3 +0,0 @@
-exports.time = function (req, res) {
-  res.status(200).json({time: Date()});
-}
